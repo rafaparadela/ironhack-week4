35afd5d24104faff066bd8413d20628359315799
Rafa Paradela <rafaparadela@gmail.com>
2014-06-27 12:10:10 +0200

    Initial Commit

---
+++ series/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  series
+//
+//  Created by Rafa Paradela on 27/06/14.
+//  Copyright (c) 2014 rafaparadela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ series/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  series
+//
+//  Created by Rafa Paradela on 27/06/14.
+//  Copyright (c) 2014 rafaparadela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
